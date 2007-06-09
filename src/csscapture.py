142f4af3399db87e11d22dde66b974ff23cf8429
cthedot <cthedot@localhost>
2007-06-09 19:34:10 +0000

    added svn keywords

--- src/csscapture.py
+++ src/csscapture.py
@@ -16,7 +16,9 @@ TODO:
 """
 __all__ = ['CSSCapture']
 __docformat__ = 'restructuredtext'
-__version__ = '0.9.1b3'
+__author__ = '$LastChangedBy$'
+__date__ = '$LastChangedDate$'
+__version__ = '0.9.2a1, SVN revision $LastChangedRevision$'
 
 import errno
 import HTMLParser
