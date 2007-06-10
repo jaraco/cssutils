db51cc4aa4c2869ba9d92718bc0669b9813150f5
cthedot <cthedot@localhost>
2007-06-10 10:24:47 +0000

    added tests for most of cssproperties contents

--- src/cssutils/tests/test_cssproperties.py
+++ src/cssutils/tests/test_cssproperties.py
@@ -3,26 +3,62 @@ __author__ = '$LastChangedBy$'
 __date__ = '$LastChangedDate$'
 __version__ = '0.9.2a1, SVN revision $LastChangedRevision$'
 
-
 import xml.dom
 
 import basetest
 
-import cssutils
+import cssutils.css
 
 
 class CSSPropertiesTestCase(basetest.BaseTestCase):
 
     def test_cssvalues(self):
-        "cssvalues"
+        "cssproperties cssvalues"
         # does actually return match object, so a very simplified test...
-        val = cssutils.css.cssproperties.cssvalues
+        match = cssutils.css.cssproperties.cssvalues
         
-        self.assertEquals(True, bool(val['color']('red')))
-        self.assertEquals(True, bool(val['top']('1px')))
-        self.assertEquals(True, bool(val['top']('0')))
-                          
-        self.assertEquals(False, bool(val['top']('red')))
+        self.assertEquals(True, bool(match['color']('red')))
+        self.assertEquals(True, bool(match['top']('1px')))
+        self.assertEquals(True, bool(match['top']('0')))
+        self.assertEquals(False, bool(match['top']('red')))
+        
+
+    def test_toDOMname(self):
+        "cssproperties _toDOMname(CSSname)"
+        _toDOMname = cssutils.css.cssproperties._toDOMname
+        
+        self.assertEquals('color', _toDOMname('color'))
+        self.assertEquals('fontStyle', _toDOMname('font-style'))
+        self.assertEquals('MozOpacity', _toDOMname('-moz-opacity'))
+        self.assertEquals('UNKNOWN', _toDOMname('UNKNOWN'))
+        self.assertEquals('AnUNKNOWN', _toDOMname('-anUNKNOWN'))
+        
+    def test_toCSSname(self):
+        "cssproperties _toCSSname(DOMname)"
+        _toCSSname = cssutils.css.cssproperties._toCSSname
+        
+        self.assertEquals('color', _toCSSname('color'))
+        self.assertEquals('font-style', _toCSSname('fontStyle'))
+        self.assertEquals('-moz-opacity', _toCSSname('MozOpacity'))
+        self.assertEquals('UNKNOWN', _toCSSname('UNKNOWN'))
+        self.assertEquals('-anUNKNOWN', _toCSSname('AnUNKNOWN'))
+        
+
+    def test_CSS2Properties(self):
+        "CSS2Properties"
+        CSS2Properties = cssutils.css.cssproperties.CSS2Properties
+        self.assertEquals(type(property()), type(CSS2Properties.color))
+        self.assertEquals(len(cssutils.css.cssproperties.cssvalues),
+                          len(CSS2Properties._properties))
+
+        c2 = CSS2Properties()
+        # CSS2Properties has simplified implementation return always None
+        self.assertEquals(None, c2.color)
+        self.assertEquals(None, c2.__setattr__('color', 1))
+        self.assertEquals(None, c2.__delattr__('color'))
+        # only defined properties
+        self.assertRaises(AttributeError, c2.__getattribute__, 'UNKNOWN')
+        
 
 if __name__ == '__main__':
     import unittest
