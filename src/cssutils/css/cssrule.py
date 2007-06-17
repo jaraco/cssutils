ee38101dc4a2ec0df1f095b16540860f7aa252b7
cthedot <cthedot@localhost>
2007-06-17 14:07:54 +0000

    removed SVN Revision comment

--- src/cssutils/css/cssrule.py
+++ src/cssutils/css/cssrule.py
@@ -3,7 +3,7 @@ __all__ = ['CSSRule']
 __docformat__ = 'restructuredtext'
 __author__ = '$LastChangedBy$'
 __date__ = '$LastChangedDate$'
-__version__ = '0.9.2a1, SVN revision $LastChangedRevision$'
+__version__ = '0.9.2a1, $LastChangedRevision$'
 
 import xml.dom
 
