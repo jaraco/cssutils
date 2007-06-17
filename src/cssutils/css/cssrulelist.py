e01486c58d2281837891048de6d93c4532861a10
cthedot <cthedot@localhost>
2007-06-17 14:07:54 +0000

    removed SVN Revision comment

--- src/cssutils/css/cssrulelist.py
+++ src/cssutils/css/cssrulelist.py
@@ -5,7 +5,7 @@ __all__ = ['CSSRuleList']
 __docformat__ = 'restructuredtext'
 __author__ = '$LastChangedBy$'
 __date__ = '$LastChangedDate$'
-__version__ = '0.9.2a1, SVN revision $LastChangedRevision$'
+__version__ = '0.9.2a1, $LastChangedRevision$'
 
 
 class CSSRuleList(list):
