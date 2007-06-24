03d187f2a34171eb8d872f3de841fc6053bbd5de
cthedot <cthedot@localhost>
2007-06-24 15:16:15 +0000

    version number up for release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ use EasyInstall or install with
 __docformat__ = 'restructuredtext'
 __author__ = '$LastChangedBy$'
 __date__ = '$LastChangedDate$'
-__version__ = '0.9.2a4'
+__version__ = '0.9.2a5'
 
 import ez_setup
 ez_setup.use_setuptools()
