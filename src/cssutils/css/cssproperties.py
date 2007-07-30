b8954743b6c7b056bb3c46a5898ecb38663784ac
cthedot <cthedot@localhost>
2007-07-30 18:44:14 +0000

    new version

--- src/cssutils/css/cssproperties.py
+++ src/cssutils/css/cssproperties.py
@@ -63,7 +63,7 @@ __all__ = ['CSS2Properties', 'cssvalues']
 __docformat__ = 'restructuredtext'
 __author__ = '$LastChangedBy$'
 __date__ = '$LastChangedDate$'
-__version__ = '0.9.2a2, $LastChangedRevision$'
+__version__ = '0.9.2b3, $LastChangedRevision$'
 
 import re
 
