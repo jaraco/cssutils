4d0f40478f9c1dca6f6b9860357737aa00922221
cthedot <cthedot@localhost>
2007-08-20 20:01:23 +0000

    cosmetical change only

--- src/cssutils/stylesheets/stylesheet.py
+++ src/cssutils/stylesheets/stylesheet.py
@@ -5,7 +5,7 @@ __all__ = ['StyleSheet']
 __docformat__ = 'restructuredtext'
 __author__ = '$LastChangedBy$'
 __date__ = '$LastChangedDate$'
-__version__ = '0.9.2a2 $LastChangedRevision$'
+__version__ = '$LastChangedRevision$'
 
 import cssutils
 
