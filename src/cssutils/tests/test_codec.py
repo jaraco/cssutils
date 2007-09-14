1941b15889e1867f0d00ee7d0d2a66e9039b5183
doerwalter <doerwalter@localhost>
2007-09-14 15:19:53 +0000

    Add decoding test: for the stateless decoder, the incremental decoder and the stream reader.

--- src/cssutils/tests/test_codec.py
+++ src/cssutils/tests/test_codec.py
@@ -7,7 +7,39 @@ __version__ = '$LastChangedRevision: 258 $'
 
 import unittest
 
+import codecs
+
 from cssutils import codec
+
+
+try:
+    codecs.lookup("utf-32")
+except LookupError:
+    haveutf32 = False
+else:
+    haveutf32 = True
+
+
+class Queue(object):
+    """
+    queue: write bytes at one end, read bytes from the other end
+    """
+    def __init__(self):
+        self._buffer = ""
+
+    def write(self, chars):
+        self._buffer += chars
+
+    def read(self, size=-1):
+        if size<0:
+            s = self._buffer
+            self._buffer = ""
+            return s
+        else:
+            s = self._buffer[:size]
+            self._buffer = self._buffer[size:]
+            return s
+
 
 class CodecTestCase(unittest.TestCase):
 
@@ -72,6 +104,72 @@ class CodecTestCase(unittest.TestCase):
 
         s = u'@charset "x"'
         self.assertEqual(codec._fixencoding(s, u"utf-8"), s.replace('"x"', '"utf-8"'))
+
+    def test_decoder(self):
+        def checkauto(encoding, input=u'@charset "x";g\xfcrk\u20ac{}'):
+            outputencoding = encoding
+            if outputencoding == "utf-8-sig":
+                outputencoding = "utf-8"
+            # Check stateless decoder
+            d = codecs.getdecoder("css")
+            self.assertEqual(d(input.encode(encoding))[0], input.replace('"x"', '"%s"' % outputencoding))
+
+            # Check stateless decoder with specified encoding
+            self.assertEqual(d(input.encode(encoding), encoding=encoding)[0], input.replace('"x"', '"%s"' % outputencoding))
+
+            # Check incremental decoder
+            id = codecs.getincrementaldecoder("css")()
+            self.assertEqual("".join(id.iterdecode(input.encode(encoding))), input.replace('"x"', '"%s"' % outputencoding))
+
+            # Check incremental decoder with specified encoding
+            id = codecs.getincrementaldecoder("css")(encoding)
+            self.assertEqual("".join(id.iterdecode(input.encode(encoding))), input.replace('"x"', '"%s"' % outputencoding))
+
+            # Check stream reader
+            q = Queue()
+            sr = codecs.getreader("css")(q)
+            result = []
+            for c in input.encode(encoding):
+                q.write(c)
+                result.append(sr.read())
+            self.assertEqual("".join(result), input.replace('"x"', '"%s"' % outputencoding))
+
+        # Autodetectable encodings
+        checkauto("utf-8-sig")
+        checkauto("utf-16")
+        checkauto("utf-16-le")
+        checkauto("utf-16-be")
+        if haveutf32:
+            checkauto("utf-32")
+            checkauto("utf-32-le")
+            checkauto("utf-32-be")
+
+        def checkdecl(encoding, input=u'@charset "%s";g\xfcrk{}'):
+            # Check stateless decoder with encoding autodetection
+            d = codecs.getdecoder("css")
+            input = input % encoding
+            self.assertEqual(d(input.encode(encoding))[0], input)
+
+            # Check stateless decoder with specified encoding
+            self.assertEqual(d(input.encode(encoding), encoding=encoding)[0], input)
+
+            # Check incremental decoder with encoding autodetection
+            id = codecs.getincrementaldecoder("css")()
+            self.assertEqual("".join(id.iterdecode(input.encode(encoding))), input)
+
+            # Check incremental decoder with specified encoding
+            id = codecs.getincrementaldecoder("css")(encoding)
+            self.assertEqual("".join(id.iterdecode(input.encode(encoding))), input)
+
+        # Use correct declaration
+        checkdecl("utf-8")
+        checkdecl("iso-8859-1", u'@charset "%s";g\xfcrk')
+        checkdecl("iso-8859-15")
+        checkdecl("cp1252")
+
+        # No recursion
+        self.assertRaises(ValueError, '@charset "css";div{}'.decode, "css")
+
 
 if __name__ == '__main__':
     import unittest
