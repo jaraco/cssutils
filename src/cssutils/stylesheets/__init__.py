c7ca60390ac83281985fec0a101ab7c437d1050c
cthedot <cthedot@localhost>
2007-09-18 21:23:43 +0000

    added MediaQuery

--- src/cssutils/stylesheets/__init__.py
+++ src/cssutils/stylesheets/__init__.py
@@ -4,19 +4,20 @@ http://www.w3.org/TR/2000/PR-DOM-Level-2-Style-20000927/stylesheets.html
 
 currently implemented:
     - MediaList
+    - MediaQuery (http://www.w3.org/TR/css3-mediaqueries/)
     - StyleSheet
     - StyleSheetList
 """
-__all__ = ['MediaList', 'StyleSheet', 'StyleSheetList']
+__all__ = ['MediaList', 'MediaQuery', 'StyleSheet', 'StyleSheetList']
 __docformat__ = 'restructuredtext'
 __author__ = '$LastChangedBy$'
 __date__ = '$LastChangedDate$'
-__version__ = '0.9.2a2 $LastChangedRevision$'
+__version__ = '$LastChangedRevision$'
 
-from medialist import MediaList
-from stylesheet import StyleSheet
-from stylesheetlist import StyleSheetList
-
+from medialist import *
+from mediaquery import *
+from stylesheet import *
+from stylesheetlist import *
 
 if __name__ == '__main__':
     for x in __all__:
