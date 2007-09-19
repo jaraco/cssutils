6dc45bd6a434a533337ff38ef9028b5e3fb1955d
cthedot <cthedot@localhost>
2007-09-19 14:04:24 +0000

    cosmetic changes

--- src/cssutils/tests/__init__.py
+++ src/cssutils/tests/__init__.py
@@ -3,46 +3,4 @@ cssutils unittests
 """
 __author__ = '$LastChangedBy$'
 __date__ = '$LastChangedDate$'
-__version__ = '0.9.2a1, $LastChangedRevision$'
-
-##import unittest
-
-##from test_tokenize import *
-##from test_cssutilsimport import *
-##from test_cssutils import *
-##from test_serialize import *
-##
-##from test_csscharsetrule import *
-##from test_csscomment import *
-##from test_cssimportrule import *
-##from test_cssrule import *
-##from test_cssrulelist import *
-##from test_cssstylerule import *
-##from test_cssunknownrule import *
-##from test_medialist import *
-
-##testcases = (
-##    TokenizerTestCase,
-##    CSSutilsImportTestCase,
-##    CSSutilsTestCase,
-##    CSSSerializerTestCase,
-##    CSSCharsetRuleTestCase,
-##    CSSCommentTestCase,
-##    CSSImportRuleTestCase,
-##    CSSRuleTestCase,
-##    CSSRuleListTestCase,
-##    CSSStyleRuleTestCase,
-##    CSSUnknownRuleTestCase,
-##    MediaListTestCase
-##    )
-
-##import logging
-##from cssutils import _log
-##_log.setloglevel(logging.FATAL)
-
-##suites = []
-##for case in testcases:
-##    suites.append(unittest.makeSuite(case))
-##
-##suite = unittest.TestSuite(tuple(suites))
-##unittest.TextTestRunner(verbosity=2).run(suite)
+__version__ = '$LastChangedRevision$'
