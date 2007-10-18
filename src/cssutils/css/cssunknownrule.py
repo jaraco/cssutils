017f051fbd4b5c19a77b83c511252d0645f963d5
cthedot <cthedot@localhost>
2007-10-18 20:45:02 +0000

    uses new tokenizer now

--- src/cssutils/css/cssunknownrule.py
+++ src/cssutils/css/cssunknownrule.py
@@ -16,21 +16,16 @@ class CSSUnknownRule(cssrule.CSSRule):
 
     Properties
     ==========
-    atkeyword
-        keyword used including @, e.g. @-moz-unknown
-    cssText: of type DOMString
-        The parsable textual representation of this rule
-    seq: a list (cssutils)
-        All parts of this rule excluding @KEYWORD but including CSSComments
-    type: see CSSRule
-        The typecode for this rule
+    inherited from CSSRule
+        - cssText
+        - type
 
     cssutils only
     -------------
     atkeyword:
         the literal keyword used
-
-    Inherits properties from CSSRule
+    seq: a list (cssutils)
+        All parts of this rule excluding @KEYWORD but including CSSComments
 
     Format
     ======
@@ -46,6 +41,7 @@ class CSSUnknownRule(cssrule.CSSRule):
         """
         super(CSSUnknownRule, self).__init__()
 
+        self.valid = True # always as unknown...
         if cssText:
             self.cssText = cssText
         else:
@@ -75,65 +71,27 @@ class CSSUnknownRule(cssrule.CSSRule):
           Raised if the rule is readonly.
         """
         super(CSSUnknownRule, self)._setCssText(cssText)
-        tokens = self._tokenize(cssText)
+        tokenizer = self._tokenize2(cssText)
+        attoken = self._nexttoken(tokenizer, None)
+        if not attoken or 'ATKEYWORD' != self._type(attoken):
+            self._log.error(u'CSSUnknownRule: No CSSUnknownRule found.',
+                            error=xml.dom.InvalidModificationErr)
+        else:
+            newatkeyword = self._tokenvalue(attoken)
+            newseq = []
+            for token in tokenizer:
+                newseq.append(self._tokenvalue(token))
 
-        if not tokens or tokens and tokens[0].type != self._ttypes.ATKEYWORD:
-            self._log.error(u'CSSUnknown: No CSSUnknown found.',
-                                error=xml.dom.InvalidModificationErr)
-            return
-           
-        newatkeyword = tokens[0].value[1:]
-        newseq = []
-        expected = ''
-        for i in range(1, len(tokens)):
-            t = tokens[i]
-            if self._ttypes.EOF == t.type:
-                break
-
-            if self._ttypes.S == t.type:
-                newseq.append(t.value) # whitespace: add
-                
-            elif self._ttypes.COMMENT == t.type:
-                # Comment: just add
-                newseq.append(cssutils.css.CSSComment(t))
-
-            # TODO: block or simple content ending with ;
-
-            elif self._ttypes.INVALID == t.type:
-                self.valid = False
-                self._log.error(u'CSSUnknown: Invalid Token found.', t)
-                return
-
-            else:
-                newseq.append(t.value)
-
-        self.atkeyword = newatkeyword
-        self.seq = newseq
+            self.atkeyword = newatkeyword
+            self.seq = newseq
 
     cssText = property(fget=_getCssText, fset=_setCssText,
         doc="(DOM) The parsable textual representation.")
     
     def __repr__(self):
-        return "cssutils.css.%s()" % (
-                self.__class__.__name__)
+        return "cssutils.css.%s(cssText=%r)" % (
+                self.__class__.__name__, self.cssText)
         
     def __str__(self):
-        return "<cssutils.css.%s object at 0x%x>" % (
-                self.__class__.__name__, id(self))
-
-
-if __name__ == '__main__':
-    c = CSSUnknownRule('@x something /*comment*/;')
-##    print c.seq
-##    print c.cssText
-##    print
-##    c.cssText = u'@x { block /*comment*/ }'
-##    print c.seq
-##    print c.cssText
-    sheet = cssutils.parseString(u'''@three-dee {
-          @background-lighting {
-            azimuth : 30deg;
-            elevation : 190deg;
-          }
-        }''')
-    print "SHEET:", sheet.cssText
+        return "<cssutils.css.%s object cssText=%r at 0x%x>" % (
+                self.__class__.__name__, self.cssText, id(self))
