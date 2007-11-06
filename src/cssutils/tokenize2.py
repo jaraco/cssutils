271f4221e4fbec49a803f66802e0ef7db3b4b61e
cthedot <cthedot@localhost>
2007-11-06 21:23:18 +0000

    resolved unicode escapes now

--- src/cssutils/tokenize2.py
+++ src/cssutils/tokenize2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """New CSS Tokenizer (a generator)
 """
 __all__ = ['Tokenizer', 'CSSProductions']
@@ -22,21 +23,6 @@ class Tokenizer(object):
     """
     _linesep = u'\n'
     
-    def __init__(self, macros=None, productions=None):
-        """
-        inits tokenizer with given macros and productions which default to
-        cssutils own macros and productions
-        """
-        self.log = cssutils.log
-        if not macros:
-            macros = MACROS
-        if not productions:
-            productions = PRODUCTIONS
-        self.tokenmatches = self._compile_productions(
-                self._expand_macros(macros, productions))
-        self.commentmatcher = [x[1] for x in self.tokenmatches if x[0] == 'COMMENT'][0]
-        self.urimatcher = [x[1] for x in self.tokenmatches if x[0] == 'URI'][0]
-
     def _expand_macros(self, macros, productions):
         """returns macro expanded productions, order of productions is kept"""
         def macro_value(m):
@@ -56,10 +42,32 @@ class Tokenizer(object):
             compiled.append((key, re.compile('^(?:%s)' % value, re.U).match))
         return compiled
 
+    def __init__(self, macros=None, productions=None):
+        """
+        inits tokenizer with given macros and productions which default to
+        cssutils own macros and productions
+        """
+        self.log = cssutils.log
+        if not macros:
+            macros = MACROS
+        if not productions:
+            productions = PRODUCTIONS
+        self.tokenmatches = self._compile_productions(
+                self._expand_macros(macros, productions))
+        self.commentmatcher = [x[1] for x in self.tokenmatches if x[0] == 'COMMENT'][0]
+        self.urimatcher = [x[1] for x in self.tokenmatches if x[0] == 'URI'][0]
+        self.unicodesub = re.compile(RE_UNICODE).sub
+
     def tokenize(self, text, fullsheet=False):
         """
-        generator: tokenizes text and yiels tokens, each token is a tuple of
+        generator: tokenizes text and yiels tokens, each token is a tuple of::
+        
             (tokenname, tokenvalue, line, col)
+        
+        The tokenvalue will contain a normal string, meaning CSS unicode 
+        escapes have been resolved to normal characters. The serializer
+        escapes needed characters back to unicode escapes depending of
+        the stylesheet target encoding.
 
         text
             to be tokenized
@@ -107,7 +115,21 @@ class Tokenizer(object):
                                     found = match.group(0)
                                     break
 
-                    yield (name, found, line, col)
+                    if name in ('URI', 'FUNCTION', 'ATKEYWORD', 'IDENT', 'STRING', 
+                                'INVALID', 'HASH', 'DIMENSION', 'COMMENT'):
+                        # may contain unicode escape, replace with normal char
+                        def repl(m):
+                            num = int(m.group(0)[1:], 16)
+                            if num < 0x10000:
+                                return unichr(num)
+                            else:
+                                return m.group(0)
+                        value = self.unicodesub(repl, found)
+                    else:
+                        # should not contain unicodes
+                        value = found
+
+                    yield (name, value, line, col)
                     text = text[len(found):]
                     nls = found.count(self._linesep)
                     line += nls
