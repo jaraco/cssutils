00c9f2cbb18a8937016067234c0433a743c643ab
cthedot <cthedot@localhost>
2008-02-02 22:51:58 +0000

    comments

--- examples/parse.py
+++ examples/parse.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from cssutils import CSSParser
+import cssutils
 import xml.dom
 
 css = '''/* This is a comment */
@@ -8,17 +8,17 @@ body {
     top: red;
     x: 1;
     }
-a { x }
+a { y }
         '''
+print "\n--- source CSS ---"
+print css
 
-print "\n--- CSSParser() ---"
-p = CSSParser()
-c1 = p.parseString(css)
+print "\n--- simple parsing ---"
+c1 = cssutils.parseString(css)
 print c1.cssText
 
-
 print "\n--- CSSParser(raiseExceptions=True) ---"
-p = CSSParser(raiseExceptions=True)
+p = cssutils.CSSParser(raiseExceptions=True)
 try:
     c2 = p.parseString(css)
 except xml.dom.DOMException, e:
