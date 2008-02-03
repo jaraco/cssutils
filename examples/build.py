dbf8f75fc424315a4d029566dc35141d7c136078
cthedot <cthedot@localhost>
2008-02-03 20:46:14 +0000

    example on google code

--- examples/build.py
+++ examples/build.py
@@ -1,42 +1,28 @@
-"""
-simple examples how to use cssutils to build a new stylesheet
-"""
-from cssutils import css, stylesheets
-sheet = css.CSSStyleSheet()
-sheet.cssText = u'@import url(example.css) tv;'
+# -*- coding: utf-8 -*-
+import cssutils
+
+css = u'''/* a comment with umlaut ä */ 
+     @namespace html "http://www.w3.org/1999/xhtml";
+     html|a { color:red; }'''
+sheet = cssutils.parseString(css)
+
+# from 0.9.5a3 iterate on sheet directly
+for rule in sheet:
+    if rule.type == rule.STYLE_RULE:
+        for property in rule.style:
+            property.value = 'green' 
+            # from 0.9.5a3 priority string is "important"
+            property.priority = 'important'
+
+# encoding from 0.9.4
+sheet.encoding = 'ascii'
+
+# namespaces from 0.9.5a3: effectively replaces prefix as same URI 
+sheet.namespaces['xhtml'] = 'http://www.w3.org/1999/xhtml'
+sheet.namespaces['atom'] = 'http://www.w3.org/2005/Atom'
+
+# add from 0.9.5a3: adds at appropriate position
+sheet.add('@import "some-other.css";')
+sheet.add('atom|title {color: #000 !important}')
+
 print sheet.cssText
-print
-
-# OUTPUT:
-# @import url(example.css) tv;
-
-style = css.CSSStyleDeclaration()
-style.setProperty(u'color', u'red')
-stylerule = css.CSSStyleRule(selectorText=u'body', style=style)
-
-#sheet.insertRule(stylerule, 0) # try before @import
-
-# OUTPUT
-# xml.dom.HierarchyRequestErr: CSSStylesheet: Found @charset, @import or @namespace
-#  before index 0.
-
-sheet.add(stylerule) # at end of rules, returns index
-1
-print sheet.cssText
-print
-
-#@import url(example.css) tv;
-#body {
-#    color: red
-#    }
-
-sheet.cssRules[0].media.appendMedium('print')
-# True
-sheet.cssRules[1].selectorList.appendSelector('a')
-#<cssutils.css.selector.Selector object at 0x00BC87D0>
-print sheet.cssText
-
-#@import url(example.css) tv, print;
-#body, a {
-#    color: red
-#    }
