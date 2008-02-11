686398027ca45816670a8af35f90f298bbe15e2b
cthedot <cthedot@localhost>
2008-02-11 21:07:47 +0000

    use simple testutil now

--- examples/build.py
+++ examples/build.py
@@ -1,28 +1,47 @@
 # -*- coding: utf-8 -*-
 import cssutils
 
-css = u'''/* a comment with umlaut ä */ 
-     @namespace html "http://www.w3.org/1999/xhtml";
-     html|a { color:red; }'''
-sheet = cssutils.parseString(css)
+EXPOUT = '''@charset "ascii";
+@import "some-other.css";
+/* a comment with umlaut \\E4  */
+@namespace xhtml "http://www.w3.org/1999/xhtml";
+@namespace atom "http://www.w3.org/2005/Atom";
+xhtml|a {
+    color: green !important
+    }
+atom|title {
+    color: #000 !important
+    }
+'''
+EXPERR = ''   
 
-# from 0.9.5a3 iterate on sheet directly
-for rule in sheet:
-    if rule.type == rule.STYLE_RULE:
-        for property in rule.style:
-            property.value = 'green' 
-            # from 0.9.5a3 priority string is "important"
-            property.priority = 'important'
+def main():
+    css = u'''/* a comment with umlaut ä */
+         @namespace html "http://www.w3.org/1999/xhtml";
+         html|a { color:red; }'''
+    sheet = cssutils.parseString(css)
+    
+    # from 0.9.5a3 iterate on sheet directly
+    for rule in sheet:
+        if rule.type == rule.STYLE_RULE:
+            for property in rule.style:
+                property.value = 'green' 
+                # from 0.9.5a3 priority string is "important"
+                property.priority = 'important'
+    
+    # encoding from 0.9.4
+    sheet.encoding = 'ascii'
+    
+    # namespaces from 0.9.5a3: effectively replaces prefix as same URI 
+    sheet.namespaces['xhtml'] = 'http://www.w3.org/1999/xhtml'
+    sheet.namespaces['atom'] = 'http://www.w3.org/2005/Atom'
+    
+    # add from 0.9.5a3: adds at appropriate position
+    sheet.add('@import "some-other.css";')
+    sheet.add('atom|title {color: #000 !important}')
+    
+    print sheet.cssText
 
-# encoding from 0.9.4
-sheet.encoding = 'ascii'
 
-# namespaces from 0.9.5a3: effectively replaces prefix as same URI 
-sheet.namespaces['xhtml'] = 'http://www.w3.org/1999/xhtml'
-sheet.namespaces['atom'] = 'http://www.w3.org/2005/Atom'
-
-# add from 0.9.5a3: adds at appropriate position
-sheet.add('@import "some-other.css";')
-sheet.add('atom|title {color: #000 !important}')
-
-print sheet.cssText
+if __name__ == '__main__':
+    main()
--- examples/selectors_tolower.py
+++ examples/selectors_tolower.py
@@ -1,27 +1,31 @@
 import cssutils
 from cssutils import css, stylesheets
 
-examplecss = u"""@charset "ascii";
-    @namespace PREfix "uri";
-    SOME > WeIrD + selector ~ used here {color: green}
-    PREfix|name {color: green}
-"""
-##
-##
+EXPOUT = '''--- ORIGINAL ---\n@charset "ascii";\n        @namespace PREfix "uri";\n        SOME > WeIrD + selector ~ used here {color: green}\n        PREfix|name {color: green}\n    \n\n--- SELECTORS TO LOWER CASE (does not simply work for PREfix|name!) ---\n--- CHANGE PREFIX (prefix is not really part of selectorText, URI is! ---\n\n@charset "ascii";\n@namespace lower-case_prefix "uri";\nsome > weird + selector ~ used here {\n    color: green\n    }\nlower-case_prefix|name {\n    color: green\n    }\n'''
+EXPERR = ''
 
-import logging
-sheet = cssutils.CSSParser(loglevel=logging.DEBUG).parseString(examplecss)
- 
+def main():
+    examplecss = u"""@charset "ascii";
+        @namespace PREfix "uri";
+        SOME > WeIrD + selector ~ used here {color: green}
+        PREfix|name {color: green}
+    """
+    
+    import logging
+    sheet = cssutils.CSSParser(loglevel=logging.DEBUG).parseString(examplecss)
+    
+    print "--- ORIGINAL ---"
+    print examplecss
+    print
+    
+    print "--- SELECTORS TO LOWER CASE (does not simply work for PREfix|name!) ---"
+    sheet.cssRules[2].selectorText = sheet.cssRules[2].selectorText.lower() 
+    
+    print "--- CHANGE PREFIX (prefix is not really part of selectorText, URI is! ---"
+    sheet.cssRules[1].prefix = 'lower-case_prefix'
+    
+    print
+    print sheet.cssText
 
-print "--- ORIGINAL ---"
-print examplecss
-print
-
-print "--- SELECTORS TO LOWER CASE (does not simply work for PREfix|name!) ---"
-sheet.cssRules[2].selectorText = sheet.cssRules[2].selectorText.lower() 
-
-print "--- CHANGE PREFIX (prefix is not really part of selectorText, URI is! ---"
-sheet.cssRules[1].prefix = 'lower-case_prefix'
-
-print
-print sheet.cssText
+if __name__ == '__main__':
+    main()
