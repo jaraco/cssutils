f2f6c7092ca0673f633bc1c87e527f5985d38648
cthedot <cthedot@localhost>
2008-03-20 18:26:34 +0000

    prepared 0.9.5b2 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ use EasyInstall or install with
 __docformat__ = 'restructuredtext'
 __author__ = 'Christof Hoeke with contributions by Walter Doerwald'
 __date__ = '$LastChangedDate::                            $:'
-__version__ = '0.9.5b1'
+__version__ = '0.9.5b2'
 
 import codecs
 import os
