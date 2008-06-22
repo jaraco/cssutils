ca71f54c660d454626601cd16924561ee9983690
cthedot <cthedot@localhost>
2008-06-22 18:41:55 +0000

    added customlog to test

--- examples/customlog.py
+++ examples/customlog.py
@@ -1,15 +1,22 @@
 import logging, StringIO
-import cssutils
 
-mylog = StringIO.StringIO()
-logging.basicConfig(stream=mylog) # here loglevel is not set
+EXPOUT = '''INFO    Property: No CSS2 Property: u'a'. [1:5: a]
 
-cssutils.log.setlog(logging.getLogger('mylog')) 
-cssutils.log.setloglevel(logging.DEBUG)
- 
-sheet = cssutils.parseString('a { x: 1; } @import "x";')
-print '--- customlog content  ---'
-print mylog.getvalue()
+'''
+EXPERR = u"Property: No CSS2 Property: u'a'. [1:5: a]\n"
+
+def main():
+    import cssutils
+    
+    st = StringIO.StringIO()
+    h = logging.StreamHandler(st)
+    h.setFormatter(logging.Formatter('%(levelname)s    %(message)s'))
+    cssutils.log.addHandler(h) 
+    ##cssutils.log.setLevel(logging.DEBUG)
+     
+    sheet = cssutils.parseString('a { a: 1; }')
+    print st.getvalue()
+
 
 # OUTPUTS:
 #--- customlog content  ---
@@ -18,3 +25,6 @@ print mylog.getvalue()
 # this value: u'1'
 #ERROR:mylog:CSSStylesheet: CSSImportRule not allowed here. [1:13: @import]
 
+
+if __name__ == '__main__':
+    main()
