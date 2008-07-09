7adc4e47a036d486c332337f83a4cddede088001
cthedot <cthedot@localhost>
2008-07-09 14:35:48 +0000

    adjusted to example on website

--- examples/customlog.py
+++ examples/customlog.py
@@ -1,30 +1,21 @@
 import logging, StringIO
 
-EXPOUT = '''INFO    Property: No CSS2 Property: u'a'. [1:5: a]
-
-'''
-EXPERR = u"Property: No CSS2 Property: u'a'. [1:5: a]\n"
+EXPOUT = ""
+EXPERR = u'''Property: No CSS2 Property: u'x'. [1:5: x]\nHTTPError opening url=u'http://example.com/x': 404 Not Found\nCSSImportRule: While processing imported style sheet href=u'http://example.com/x': IOError('Cannot read Stylesheet or it is empty.',)\nCSSStylesheet: CSSImportRule not allowed here. [1:13: @import]\n'''
 
 def main():
     import cssutils
-    
-    st = StringIO.StringIO()
-    h = logging.StreamHandler(st)
+
+    mylog = StringIO.StringIO()
+    h = logging.StreamHandler(mylog)
     h.setFormatter(logging.Formatter('%(levelname)s    %(message)s'))
-    cssutils.log.addHandler(h) 
-    ##cssutils.log.setLevel(logging.DEBUG)
-     
-    sheet = cssutils.parseString('a { a: 1; }')
-    print st.getvalue()
+    #cssutils.log.addHandler(h)
+    cssutils.log.setLevel(logging.INFO)
 
+    sheet = cssutils.parseString('a { x: 1; } @import "http://example.com/x";')
+    #print mylog.getvalue()
 
-# OUTPUTS:
-#--- customlog content  ---
-#INFO:mylog:Property: No CSS2 Property: "x".
-#INFO:mylog:CSSValue: Unable to validate as no or unknown property context set for
-# this value: u'1'
-#ERROR:mylog:CSSStylesheet: CSSImportRule not allowed here. [1:13: @import]
-
+    cssutils.log.removeHandler(h)
 
 if __name__ == '__main__':
     main()
