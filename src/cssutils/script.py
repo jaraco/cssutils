643bd191e9167573ab4e164d04351bab5c5ace91
cthedot <cthedot@localhost>
2008-07-23 21:04:30 +0000

    using cssutils.log instead of sys.stderr now

--- src/cssutils/script.py
+++ src/cssutils/script.py
@@ -111,7 +111,7 @@ class CSSCapture(object):
         if self._ua:
             req.add_header('User-agent', self._ua)
             self._log.info('        * Using User-Agent: %s', self._ua)
-            
+
         try:
             res = urllib2.urlopen(req)
         except urllib2.HTTPError, e:
@@ -126,15 +126,15 @@ class CSSCapture(object):
 
         return url, res
 
-    def _createStyleSheet(self, href=None, 
-                          media=None, 
-                          parentStyleSheet=None, 
+    def _createStyleSheet(self, href=None,
+                          media=None,
+                          parentStyleSheet=None,
                           title=u'',
                           cssText=None,
                           encoding=None):
         """
         Return CSSStyleSheet read from href or if cssText is given use that.
-        
+
         encoding
             used if inline style found, same as self.docencoding
         """
@@ -144,7 +144,7 @@ class CSSCapture(object):
 
         sheet = self._cssparser.parseString(cssText, href=href, media=media, title=title,
                                             encoding=encoding)
-        
+
         if not sheet:
             return None
 
@@ -165,10 +165,10 @@ class CSSCapture(object):
         """
         # TODO: ownerNode should be set to the <link> node
         self._htmlparser.feed(doctext)
-        
+
         for typ, data in self._htmlparser.sheets:
             sheet = None
-            
+
             if LINK == typ:
                 self._log.info(u'+ PROCESSING <link> %r' % data)
 
@@ -188,8 +188,8 @@ class CSSCapture(object):
                                                encoding=self.docencoding)
                 if sheet:
                     sheet._href = None # inline have no href!
-                print sheet.cssText 
-                                               
+                print sheet.cssText
+
             if sheet:
                 self.stylesheetlist.append(sheet)
                 self._doImports(sheet, base=docurl)
@@ -201,12 +201,12 @@ class CSSCapture(object):
         found CSS stylesheets are appended to stylesheetlist
         """
         # TODO: only if not parsed these have to be read extra!
-        
+
         for rule in parentStyleSheet.cssRules:
             if rule.type == rule.IMPORT_RULE:
                 self._log.info(u'+ PROCESSING @import:')
                 self._log.debug(u'    IN: %s\n' % parentStyleSheet.href)
-                sheet = rule.styleSheet                
+                sheet = rule.styleSheet
                 href = urlparse.urljoin(base, rule.href)
                 if sheet:
                     self._log.info(u'    %s\n' % sheet)
@@ -215,7 +215,7 @@ class CSSCapture(object):
 
     def capture(self, url):
         """
-        Capture all stylesheets at given URL's HTML document. 
+        Capture all stylesheets at given URL's HTML document.
         Any HTTPError is raised to caller.
 
         url
@@ -226,7 +226,7 @@ class CSSCapture(object):
         self._log.info(u'\nCapturing CSS from URL:\n    %s\n', url)
         self._nonparsed = {}
         self.stylesheetlist = cssutils.stylesheets.StyleSheetList()
-            
+
         # used to save inline styles
         scheme, loc, path, query, fragment = urlparse.urlsplit(url)
         self._filename = os.path.basename(path)
@@ -235,9 +235,9 @@ class CSSCapture(object):
         url, res = self._doRequest(url)
         if not res:
             sys.exit(1)
-            
+
         rawdoc = res.read()
-            
+
         self.docencoding = encutils.getEncodingInfo(
             res, rawdoc, log=self._log).encoding
         self._log.info(u'\nUsing Encoding: %s\n', self.docencoding)
@@ -260,7 +260,7 @@ class CSSCapture(object):
             save literal CSS from server or save the parsed CSS
         minified
             save minified CSS
-            
+
         Both parsed and minified (which is also parsed of course) will
         loose information which cssutils is unable to understand or where
         it is simple buggy. You might to first save the raw version before
@@ -272,14 +272,14 @@ class CSSCapture(object):
         if minified:
             cssutils.ser.prefs.useMinified()
             msg = 'minified'
-        
+
         inlines = 0
         for i, sheet in enumerate(self.stylesheetlist):
             url = sheet.href
             if not url:
                 inlines += 1
                 url = u'%s_INLINE_%s.css' % (self._filename, inlines)
-                
+
             # build savepath
             scheme, loc, path, query, fragment = urlparse.urlsplit(url)
             # no absolute path
@@ -297,7 +297,7 @@ class CSSCapture(object):
                 self._log.debug(u'Path "%s" already exists.', savepath)
 
             self._log.info(u'SAVING %s, %s %r' % (i+1, msg, savefn))
-            
+
             sf = open(savefn, 'wb')
             if saveraw:
                 cssText = self._nonparsed[sheet]
@@ -325,34 +325,41 @@ into a single new sheet.
         `minify`
             defines if the combined sheet should be minified, default True
     """
-    sys.stderr.write('COMBINING %s\n' % proxypath)
-    
+    log = cssutils.log
+
+    log.info('Combining files in proxy %r' % proxypath, neverraise=True)
+    #sys.stderr.write('COMBINING %s\n' % proxypath)
+
     if sourceencoding is not None:
-        sys.stderr.write('USING SOURCE ENCODING: %s\n' % sourceencoding)
-        
+        log.info('Using source encoding %r' % sourceencoding,
+                  neverraise=True)
+        #sys.stderr.write('USING SOURCE ENCODING: %s\n' % sourceencoding)
+
     src = cssutils.parseFile(proxypath, encoding=sourceencoding)
     srcpath = os.path.dirname(proxypath)
     combined = cssutils.css.CSSStyleSheet()
     for rule in src.cssRules:
         if rule.type == rule.IMPORT_RULE:
             fn = os.path.join(srcpath, rule.href)
-            sys.stderr.write('* PROCESSING @import %s\n' % fn)
+            log.info('Processing @import %r' % fn,
+                     neverraise=True)
             importsheet = cssutils.parseFile(fn, encoding=sourceencoding)
             importsheet.encoding = None # remove @charset
             combined.add(cssutils.css.CSSComment(cssText=u'/* %s */' %
                                                  rule.cssText))
             for x in importsheet.cssRules:
                 if x.type == x.IMPORT_RULE:
-                    sys.stderr.write('INFO\tNested @imports are not combined: %s\n' % x.cssText)
+                    log.info('Nested @imports are not combined: %s' % x.cssText,
+                              neverraise=True)
 
                 combined.add(x)
 
         else:
             combined.add(rule)
 
-    sys.stderr.write('SETTING TARGET ENCODING: %s\n' % targetencoding)
+    log.info('Setting target encoding %r' % targetencoding, neverraise=True)
     combined.encoding = targetencoding
-    
+
     if minify:
         # save old setting and use own serializer
         oldser = cssutils.ser
@@ -362,5 +369,5 @@ into a single new sheet.
         cssutils.setSerializer(oldser)
     else:
         cssText = combined.cssText
-        
+
     return cssText
