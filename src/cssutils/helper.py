ec25d8082dd04aa01d6583ef125de8d4dfd47606
cthedot <cthedot@localhost>
2008-08-09 19:27:32 +0000

    added normalize function

--- src/cssutils/helper.py
+++ src/cssutils/helper.py
@@ -1,8 +1,10 @@
 """cssutils helper
 """
-__all__ = ['Deprecated']
+__all__ = ['Deprecated', 'normalize']
 __docformat__ = 'restructuredtext'
 __version__ = '$Id: errorhandler.py 1234 2008-05-22 20:26:12Z cthedot $'
+
+import re
 
 class Deprecated(object):
     """This is a decorator which can be used to mark functions
@@ -27,3 +29,23 @@ class Deprecated(object):
         newFunc.__doc__ = func.__doc__
         newFunc.__dict__.update(func.__dict__)
         return newFunc
+
+# simple escapes, all non unicodes
+_simpleescapes = re.compile(ur'(\\[^0-9a-fA-F])').sub
+        
+def normalize(x):
+    """
+    normalizes x, namely:
+
+    - remove any \ before non unicode sequences (0-9a-zA-Z) so for
+      x=="c\olor\" return "color" (unicode escape sequences should have
+      been resolved by the tokenizer already)
+    - lowercase
+    """
+    if x:
+        def removeescape(matchobj):
+            return matchobj.group(0)[1:]
+        x = _simpleescapes(removeescape, x)
+        return x.lower()
+    else:
+        return x
