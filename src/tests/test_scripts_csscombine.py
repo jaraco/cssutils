dc4fcf0792fa4268bf149f5e4c0ab3bf46d217fa
cthedot <cthedot@localhost>
2008-12-24 15:20:34 +0000

    csscombine works with URL now and combines nested @imports

--- src/tests/test_scripts_csscombine.py
+++ src/tests/test_scripts_csscombine.py
@@ -1,33 +1,35 @@
 """Testcases for cssutils.scripts.csscombine"""
 __version__ = '$Id$'
 
-import os
-import basetest
 from cssutils.scripts import csscombine
+import basetest
 import cssutils
+import os
+import urllib
 
 class CSSCombine(basetest.BaseTestCase):
 
+    C = '@namespace s2"uri";s2|sheet-1{top:1px}s2|sheet-2{top:2px}proxy{top:3px}' 
+
+
     def test_combine(self):
-        "scripts.csscombine"
+        "scripts.csscombine"        
+        # path, SHOULD be keyword argument!
         csspath = os.path.join(os.path.dirname(__file__), '..', '..', 
                                'sheets', 'csscombine-proxy.css')
         combined = csscombine(csspath)
-        self.assertEqual(combined, 
-                         '@charset "utf-8";@import"1.css";@namespace s2"uri";s2|sheet-1{top:1px}s2|sheet-2{top:2px}proxy{top:3px}' 
-                         )
-        combined = csscombine(csspath, targetencoding='ascii')
-        self.assertEqual(combined, 
-                         '@charset "ascii";@import"1.css";@namespace s2"uri";s2|sheet-1{top:1px}s2|sheet-2{top:2px}proxy{top:3px}' 
-                         )
+        self.assertEqual(combined, self.C)
+        combined = csscombine(path=csspath, targetencoding='ascii')
+        self.assertEqual(combined, '@charset "ascii";' + self.C)
+
+        # url
+        cssurl = u'file:' + urllib.pathname2url(os.path.abspath(csspath))
+        combined = csscombine(url=cssurl)
+        self.assertEqual(combined, self.C)
+        combined = csscombine(url=cssurl, targetencoding='ascii')
+        self.assertEqual(combined, '@charset "ascii";' + self.C)
 
         cssutils.log.raiseExceptions = True 
-
-    def tearDown(self):
-        # needs to be re-enabled here for other tests
-        cssutils.log.raiseExceptions = True
-        # no needed as csscombine uses own serializer
-        # cssutils.ser.prefs.useDefaults() 
 
 
 if __name__ == '__main__':
