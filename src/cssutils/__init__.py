e69f8b3c6af5a6ac60f1b36e98c2c67709450baf
cthedot <cthedot@localhost>
2009-06-09 18:31:56 +0000

    prepared 0.9.6b1 release

--- src/cssutils/__init__.py
+++ src/cssutils/__init__.py
@@ -72,7 +72,7 @@ __docformat__ = 'restructuredtext'
 __author__ = 'Christof Hoeke with contributions by Walter Doerwald'
 __date__ = '$LastChangedDate::                            $:'
 
-VERSION = '0.9.6a5'
+VERSION = '0.9.6b1'
 
 __version__ = '%s $Id$' % VERSION
 
