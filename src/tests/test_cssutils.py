18998e585afa49cd87f77550bcaedfd55df2e2cf
cthedot <cthedot@localhost>
2009-08-01 22:10:11 +0000

    prepared for release

--- src/tests/test_cssutils.py
+++ src/tests/test_cssutils.py
@@ -25,7 +25,7 @@ a {
 /* import.css*/'''
 
     def test_VERSION(self):
-        self.assertEqual('0.9.6b2', cssutils.VERSION)
+        self.assertEqual('0.9.6b3', cssutils.VERSION)
 
     def test_parseString(self):
         "cssutils.parseString()"
