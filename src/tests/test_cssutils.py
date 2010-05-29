1bf51fa21d8b6eda2fc543e5b00432eb69b00f70
cthedot <cthedot@localhost>
2010-05-29 18:34:06 +0000

    update version

--- src/tests/test_cssutils.py
+++ src/tests/test_cssutils.py
@@ -25,7 +25,7 @@ class CSSutilsTestCase(basetest.BaseTestCase):
     }'''
 
     def test_VERSION(self):
-        self.assertEqual('0.9.7a6', cssutils.VERSION)
+        self.assertEqual('0.9.7b1', cssutils.VERSION)
 
     def test_parseString(self):
         "cssutils.parseString()"
