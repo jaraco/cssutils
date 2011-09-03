b1b7a55387407e6f9c915f600aa7c891e321bc7d
cthedot <cthedot@localhost>
2011-09-03 19:52:25 +0200

    convert examples too now

--- 2to3.bat
+++ 2to3.bat
@@ -1,7 +1,10 @@
 REM set PY3 to your Python 3 exe
 REM set PY3PATH to your Python 3 install dir
 REM (tested with py3.2 64)
+REM  -f idioms
+
+xcopy /e examples examples3
+%PY3% %PY3PATH%\tools\scripts\2to3.py --no-diffs -nw examples3
 
 xcopy /e src src3
-REM  -f idioms
 %PY3% %PY3PATH%\tools\scripts\2to3.py --no-diffs -nw src3
