efc73b5f5e38a3fc15afdb3dc2d6c4dec89d9d9e
cthedot <cthedot@gmail.com>
2013-11-28 22:27:14 +0100

    Fixes issue #20 for Py 3.x

--- src/cssutils/_fetch.py
+++ src/cssutils/_fetch.py
@@ -17,28 +17,28 @@ def _defaultFetcher(url):
 
     Returns ``(encoding, string)`` or ``None``
     """
-    try:        
+    try:
         request = urllib2.Request(url)
-        request.add_header('User-agent', 
+        request.add_header('User-agent',
                            'cssutils %s (http://www.cthedot.de/cssutils/)' % VERSION)
         res = urllib2.urlopen(request)
-    except OSError, e:
-        # e.g if file URL and not found
-        log.warn(e, error=OSError)
-    except (OSError, ValueError), e:
-        # invalid url, e.g. "1"
-        log.warn(u'ValueError, %s' % e.args[0], error=ValueError)
     except urllib2.HTTPError, e:
         # http error, e.g. 404, e can be raised
-        log.warn(u'HTTPError opening url=%s: %s %s' % 
+        log.warn(u'HTTPError opening url=%s: %s %s' %
                           (url, e.code, e.msg), error=e)
     except urllib2.URLError, e:
         # URLError like mailto: or other IO errors, e can be raised
         log.warn(u'URLError, %s' % e.reason, error=e)
+    except OSError, e:
+        # e.g if file URL and not found
+        log.warn(e, error=OSError)
+    except ValueError, e:
+        # invalid url, e.g. "1"
+        log.warn(u'ValueError, %s' % e.args[0], error=ValueError)
     else:
         if res:
             mimeType, encoding = encutils.getHTTPInfo(res)
             if mimeType != u'text/css':
-                log.error(u'Expected "text/css" mime type for url=%r but found: %r' % 
+                log.error(u'Expected "text/css" mime type for url=%r but found: %r' %
                                   (url, mimeType), error=ValueError)
             return encoding, res.read()
