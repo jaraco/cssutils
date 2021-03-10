7ac2ad47d339990ba5d8f4409059c80b11c9cb64
Jason R. Coombs <jaraco@jaraco.com>
2021-03-10 07:49:35 -0500

    Update import test not to rely on globals.

--- cssutils/tests/test_cssutilsimport.py
+++ cssutils/tests/test_cssutilsimport.py
@@ -1,35 +1,23 @@
 """Testcase for cssutils imports"""
 
-before = len(locals())  # to check is only exp amount is imported
-from cssutils import *
-
-after = len(locals())  # to check is only exp amount is imported
-
 import unittest
+
+import cssutils
 
 
 class CSSutilsImportTestCase(unittest.TestCase):
     def test_import_all(self):
         "from cssutils import *"
-        import cssutils
-
-        act = globals()
+        namespace = {}
+        exec('from cssutils import *', namespace)
+        del namespace['__builtins__']
         exp = {
-            'CSSParser': CSSParser,
-            'CSSSerializer': CSSSerializer,
+            'CSSParser': cssutils.CSSParser,
+            'CSSSerializer': cssutils.CSSSerializer,
             'css': cssutils.css,
             'stylesheets': cssutils.stylesheets,
         }
-        exptotal = before + len(exp) + 1
-        # imports before + * + "after"
-        self.assertTrue(after == exptotal, 'too many imported')
-
-        found = 0
-        for e in exp:
-            self.assertTrue(e in act, '%s not found' % e)
-            self.assertTrue(act[e] == exp[e], '%s not the same' % e)
-            found += 1
-        self.assertTrue(found == len(exp))
+        assert namespace == exp
 
 
 if __name__ == '__main__':
