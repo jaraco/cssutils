25d8a25a447a80b60b5a1937fd41cb74f7318e7f
Jason R. Coombs <jaraco@jaraco.com>
2021-03-09 22:32:31 -0500

    👹 Feed the hobgoblins (delint).

--- cssutils/stylesheets/__init__.py
+++ cssutils/stylesheets/__init__.py
@@ -3,7 +3,7 @@ http://www.w3.org/TR/2000/PR-DOM-Level-2-Style-20000927/stylesheets.html
 """
 __all__ = ['MediaList', 'MediaQuery', 'StyleSheet', 'StyleSheetList']
 
-from .medialist import *
-from .mediaquery import *
-from .stylesheet import *
-from .stylesheetlist import *
+from .medialist import MediaList
+from .mediaquery import MediaQuery
+from .stylesheet import StyleSheet
+from .stylesheetlist import StyleSheetList
