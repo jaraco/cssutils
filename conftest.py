cdffe59ac62e6e7dd0240e54f4952f271df46230
Jason R. Coombs <jaraco@jaraco.com>
2021-04-05 17:24:54 -0400

    Add test to ensure tests are hermetic w.r.t. profiles and add fixture to make hermetic tests that alter profiles. Fixes #4.

--- conftest.py
+++ conftest.py
@@ -1,4 +1,25 @@
+import pytest
+
+import cssutils
+
+
 collect_ignore = [
     'cssutils/_fetchgae.py',
     'tools',
 ]
+
+
+@pytest.fixture(autouse=True)
+def hermetic_profiles():
+    """
+    Ensure that tests are hermetic w.r.t. profiles.
+    """
+    before = list(cssutils.profile.profiles)
+    yield
+    assert before == cssutils.profile.profiles
+
+
+@pytest.fixture
+def saved_profiles(monkeypatch):
+    profiles = cssutils.profiles.Profiles(log=cssutils.log)
+    monkeypatch.setattr(cssutils, 'profile', profiles)
