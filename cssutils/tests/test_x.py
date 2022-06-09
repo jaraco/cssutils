ff57cb55a334696f72a4bb72cb319b68df8329e6
Jason R. Coombs <jaraco@jaraco.com>
2022-06-08 20:59:49 -0400

    unittest2pytest

--- cssutils/tests/test_x.py
+++ cssutils/tests/test_x.py
@@ -6,10 +6,8 @@ import pytest
 
 import cssutils
 
-from . import basetest
 
-
-class XTestCase(basetest.BaseTestCase):
+class XTestCase:
     def setUp(self):
         cssutils.ser.prefs.useDefaults()
 
@@ -20,32 +18,32 @@ class XTestCase(basetest.BaseTestCase):
     def test_priority(self):
         "Property.priority"
         s = cssutils.parseString('a { color: red }')
-        self.assertEqual(s.cssText, 'a {\n    color: red\n    }'.encode())
+        assert s.cssText == 'a {\n    color: red\n    }'.encode()
 
-        self.assertEqual('', s.cssRules[0].style.getPropertyPriority('color'))
+        assert '' == s.cssRules[0].style.getPropertyPriority('color')
 
         s = cssutils.parseString('a { color: red !important }')
-        self.assertEqual('a {\n    color: red !important\n    }', s.cssText)
-        self.assertEqual('important', s.cssRules[0].style.getPropertyPriority('color'))
+        assert 'a {\n    color: red !important\n    }' == s.cssText
+        assert 'important' == s.cssRules[0].style.getPropertyPriority('color')
 
         cssutils.log.raiseExceptions = True
         p = cssutils.css.Property('color', 'red', '')
-        self.assertEqual(p.priority, '')
+        assert p.priority == ''
         p = cssutils.css.Property('color', 'red', '!important')
-        self.assertEqual(p.priority, 'important')
+        assert p.priority == 'important'
         self.assertRaisesMsg(
             xml.dom.SyntaxErr, '', cssutils.css.Property, 'color', 'red', 'x'
         )
 
         cssutils.log.raiseExceptions = False
         p = cssutils.css.Property('color', 'red', '!x')
-        self.assertEqual(p.priority, 'x')
+        assert p.priority == 'x'
         p = cssutils.css.Property('color', 'red', '!x')
-        self.assertEqual(p.priority, 'x')
+        assert p.priority == 'x'
         cssutils.log.raiseExceptions = True
 
         # invalid but kept!
         # cssutils.log.raiseExceptions = False
         s = cssutils.parseString('a { color: red !x }')
-        self.assertEqual('a {\n    color: red !x\n    }', s.cssText)
-        self.assertEqual('x', s.cssRules[0].style.getPropertyPriority('color'))
+        assert 'a {\n    color: red !x\n    }' == s.cssText
+        assert 'x' == s.cssRules[0].style.getPropertyPriority('color')
