685783b2c185df41870cfecb2ebbeaf6ff0b7931
Jason R. Coombs <jaraco@jaraco.com>
2022-06-08 20:58:43 -0400

    Uncomment code and mark it as xfail.

--- cssutils/tests/test_x.py
+++ cssutils/tests/test_x.py
@@ -1,7 +1,12 @@
 """Testcases for cssutils.css.CSSValue and CSSPrimitiveValue."""
 
-from . import basetest
+import xml.dom
+
+import pytest
+
 import cssutils
+
+from . import basetest
 
 
 class XTestCase(basetest.BaseTestCase):
@@ -11,39 +16,36 @@ class XTestCase(basetest.BaseTestCase):
     def tearDown(self):
         cssutils.ser.prefs.useDefaults()
 
-    def test_prioriy(self):
+    @pytest.mark.xfail(reason="not implemented")
+    def test_priority(self):
         "Property.priority"
         s = cssutils.parseString('a { color: red }')
         self.assertEqual(s.cssText, 'a {\n    color: red\n    }'.encode())
 
+        self.assertEqual('', s.cssRules[0].style.getPropertyPriority('color'))
 
-#        self.assertEqual(u'', s.cssRules[0].style.getPropertyPriority('color'))
-#
-#        s = cssutils.parseString('a { color: red !important }')
-#        self.assertEqual(u'a {\n    color: red !important\n    }', s.cssText)
-#        self.assertEqual(
-#            u'important', s.cssRules[0].style.getPropertyPriority('color'))
-#
-#        cssutils.log.raiseExceptions = True
-#        p = cssutils.css.Property(u'color', u'red', u'')
-#        self.assertEqual(p.priority, u'')
-#        p = cssutils.css.Property(u'color', u'red', u'!important')
-#        self.assertEqual(p.priority, u'important')
-#        self.assertRaisesMsg(xml.dom.SyntaxErr,
-#                             u'',
-#                             cssutils.css.Property, u'color', u'red', u'x')
-#
-#        cssutils.log.raiseExceptions = False
-#        p = cssutils.css.Property(u'color', u'red', u'!x')
-#        self.assertEqual(p.priority, u'x')
-#        p = cssutils.css.Property(u'color', u'red', u'!x')
-#        self.assertEqual(p.priority, u'x')
-#        cssutils.log.raiseExceptions = True
-#
-#
-#        # invalid but kept!
-#        #cssutils.log.raiseExceptions = False
-#        s = cssutils.parseString('a { color: red !x }')
-#        self.assertEqual(u'a {\n    color: red !x\n    }', s.cssText)
-#        self.assertEqual(u'x', s.cssRules[0].style.getPropertyPriority('color'))
-#
+        s = cssutils.parseString('a { color: red !important }')
+        self.assertEqual('a {\n    color: red !important\n    }', s.cssText)
+        self.assertEqual('important', s.cssRules[0].style.getPropertyPriority('color'))
+
+        cssutils.log.raiseExceptions = True
+        p = cssutils.css.Property('color', 'red', '')
+        self.assertEqual(p.priority, '')
+        p = cssutils.css.Property('color', 'red', '!important')
+        self.assertEqual(p.priority, 'important')
+        self.assertRaisesMsg(
+            xml.dom.SyntaxErr, '', cssutils.css.Property, 'color', 'red', 'x'
+        )
+
+        cssutils.log.raiseExceptions = False
+        p = cssutils.css.Property('color', 'red', '!x')
+        self.assertEqual(p.priority, 'x')
+        p = cssutils.css.Property('color', 'red', '!x')
+        self.assertEqual(p.priority, 'x')
+        cssutils.log.raiseExceptions = True
+
+        # invalid but kept!
+        # cssutils.log.raiseExceptions = False
+        s = cssutils.parseString('a { color: red !x }')
+        self.assertEqual('a {\n    color: red !x\n    }', s.cssText)
+        self.assertEqual('x', s.cssRules[0].style.getPropertyPriority('color'))
